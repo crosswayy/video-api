e1597ef956634742e3d02cab76de735334e36dbf
Yauhen Dubina <yauhen.dubina@innowise-group.com>
2022-04-09 00:41:51 +0300

    Before docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, VideoModule],
   controllers: [],
   providers: [],
 })
