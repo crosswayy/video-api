e431f3d36b5d39a7aba5135da3cc555f5d5d7822
Yauhen Dubina <yauhen.dubina@innowise-group.com>
2022-04-12 19:18:14 +0300

    Add hashedRT column to user entity

---
+++ src/database/migrations/1649769161668-Add hashedRT column.ts
@@ -0,0 +1,19 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddHashedRTColumn1649769161668 implements MigrationInterface {
+    name = 'AddHashedRTColumn1649769161668'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE "user"
+            ADD "hashedRT" character varying
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE "user" DROP COLUMN "hashedRT"
+        `);
+    }
+
+}
